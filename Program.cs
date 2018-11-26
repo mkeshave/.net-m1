4c81e7e34ddff79ddfb26a52a3a47e3cef79f5f8
mkeshave <43692822+mkeshave@users.noreply.github.com>
2018-11-26 17:31:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rollNumber;
+                string studentName, address;
+                byte age;
+                char gender;
+                DateTime dob;
+                float percentage;
+            Student obj = new Student();
+            Console.WriteLine("Enter number of students");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Student[] arr = new Student[n];
+            for (int i = 0; i<= n; i++)
+            {
+                Console.WriteLine("Enter Details");
+                rollNumber = Convert.ToInt32(Console.ReadLine());
+                studentName = Console.ReadLine();
+                age= Convert.ToByte(Console.ReadLine());
+                gender = Convert.ToChar(Console.ReadLine());
+                dob = Convert.ToDateTime(Console.ReadLine());
+                percentage= float.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Q4
+{
+    class Student
+    {
+        int rollNumber;
+        string studentName, address;
+        byte age;
+        char gender;
+        DateTime dob;
+        float percentage;
+
+        public void studentDetails(
+        int rollNumber,
+        string studentName, string address,
+        byte age,
+        char gender,
+        DateTime dob,
+        float percentage)
+        {
+
+        }
+    }
+}
